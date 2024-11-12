8a1098a07209b8a728ad18366fd27f8dc47e298c
cdf-sys <charles.french621@gmail.com>
2024-11-12 16:23:47 -0500

    Fixed issue with swap_song_id3_tags

--- src/tags/id3_handler.py
+++ src/tags/id3_handler.py
@@ -1,4 +1,4 @@
-from mutagen.id3 import ID3, ID3NoHeaderError
+from mutagen.id3 import ID3, ID3NoHeaderError, TKEY, COMM
 import os
 
 
@@ -33,11 +33,35 @@ def find_songs_by_id3_tag(directory, tag, tag_value):
 
 def swap_song_id3_tags(filename, first_tag, second_tag):
     song = find_song(filename)
-    if song is None or (song[first_tag] is None and song[second_tag] is None):
+    if song is None:
         return None
     else:
-        temp_tag = song[first_tag].text[0]
-        song[first_tag].text[0] = song[second_tag].text[0]
-        song[second_tag].text[0] = temp_tag
+        try:
+            temp_tag1 = song[first_tag].text[0]
+        except KeyError:
+            temp_tag1 = ""
+            if first_tag.find("eng") != -1:
+                song.add(COMM(encoding=3, lang='eng', text=temp_tag1))
+                song.save()
+                song[COMM] = COMM(encoding=3, lang='eng', text=temp_tag1)
+            else:
+                song.add(first_tag)
+                song[first_tag] = first_tag(encoding=3, text=temp_tag1)
+            song.save()
+        try:
+            temp_tag2 = song[second_tag].text[0]
+        except KeyError:
+            temp_tag2 = ""
+            if second_tag.find("eng") != -1:
+                song.add(COMM(encoding=3, lang='eng', text=temp_tag2))
+                song.save()
+                song[COMM] = COMM(encoding=3, lang='eng', text=temp_tag2)
+            else:
+                song.add(second_tag)
+                song[second_tag] = second_tag(encoding=3, text=temp_tag2)
+            song.save()
+        print(song.keys())
+        song[first_tag].text[0] = temp_tag2
+        song[second_tag].text[0] = temp_tag1
         song.save()
         return song
